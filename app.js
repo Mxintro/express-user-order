d180cc8d0650e114df9c18ae98eb5c894c7883db
Mxintro <986695652@qq.com>
2020-12-06 00:40:11 +0800

    register

--- app.js
+++ app.js
@@ -1,19 +1,38 @@
 const express = require('express')
 const app = express()
 const port = 3000
+
 const bodyParser = require('body-parser')
 const multer = require('multer') // v1.0.5
 const upload = multer() // for parsing multipart/form-data
 
+const jwt = require('jsonwebtoken')
+const utils = require('./utils.js')
+
 app.use(bodyParser.json()) // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })) 
+app.use(function(req, res, next){
+  //设置跨域访问
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+
+  if (req.method == 'OPTIONS') {
+    res.sendStatus(200); /*让options请求快速返回*/
+  }else {
+    next();
+  }
+})
+
+app.listen(port, () => console.log(`app listening on port ${port}`)) 
 
 const connection = require('./db')
 connection.connect()
 
 const dbQuery = {
 	insert:'insert into user(id, name, phone, password) VALUES(0,?,?,?)',
-	getUserByPhone:'select * from user where phone = ?',
+  getUserByPhone:'select * from user where phone = ?',
+  getUserById: 'select * from user where id = ?',
   queryPwd:'select * from user where password = ?',
   orderInsert:'insert into order(id, name, uid, good, price,count,cost) VALUES(0,?,?,?,?,?,?)',
   orderQuery: 'slect * from order where uid = ?'
@@ -21,27 +40,38 @@ const dbQuery = {
 
 app.post('/login', upload.array(), (req, res) => {
   const params = req.body
-  connection.query(dbQuery.query, params.phone, (err, result) => {
+  console.log(params);
+  connection.query(dbQuery.getUserByPhone, params.phone, (err, result) => {
     console.log(result);
-    if (err) throw err
+    if (err) {
+      res.send({
+        status: 500,
+        msg: err
+      })
+      res.end()
+    }
     else {
       if (result.length == 0) {
         res.send({
-          status: 1,
+          status: 401,
           msg: '该手机号尚未注册.'
         })
         res.end()
       } else {
-        const response = result[0]
-        if (response.phone == params.phone && response.password == params.password) {
+        const user = result[0]
+        if (user.phone == params.phone && user.password == params.password) {
+          const data = {...user}
+          delete data.password
+          data.token = jwt.sign({uid: user.id}, 'seaturtle', { expiresIn: 60 * 60 })
           res.send({
-            status: 0,
-            msg: '恭喜,登录成功.'
+            status: 200,
+            msg: '恭喜,登录成功.',
+            data: data
           })
           res.end()
         } else {
           res.send({
-            status: 2,
+            status: 401,
             msg: '手机号或者密码有误.'
           })
           res.end()
@@ -54,30 +84,51 @@ app.post('/login', upload.array(), (req, res) => {
 app.post('/reg', upload.array(), (req, res) => {
   const params = req.body
   connection.query(dbQuery.getUserByPhone, params.phone, (err, result) => {
-    console.log("++++++++++++");
-    if (err) throw err
+    if (err) {
+      res.send({
+        status: 500,
+        msg: err
+      })
+      res.end()
+    }
     else {
       //从数据库 查询 手机号 有没有注册
       if (result.length != 0) {
         res.send({
-          status: 5,
+          status: 403,
           msg: '该手机号已经被注册'
         })
         res.end()
       } else if (result.length == 0) {
-        var sql = dbQuery.insert;
-        let userInfo = [params.name, params.phone, params.password];
-        connection.query(sql, userInfo, (err, result) => {
-          if (err) throw err
-          else {
-            console.log("++++++++++++");
+        let userInfo = [params.name, params.phone, params.password]
+        connection.query(dbQuery.insert, userInfo, (err, result) => {
+          if (err) {
             res.send({
-              params,
-              status: 0,
-              message: '恭喜,注册成功.',
-              result
-            });
-            res.end();
+              status: 500,
+              msg: err
+            })
+            res.end()
+          }
+          else {
+            console.log(result.insertId)
+            connection.query(dbQuery.getUserById, result.insertId, (err, result) => {
+              if (err) {
+                res.send({
+                  status: 500,
+                  msg: err
+                })
+                res.end()
+              }
+              const user = result[0]
+              const data = {...user}
+              delete data.password
+              data.token = jwt.sign({uid: user.id}, 'seaturtle', { expiresIn: 60 * 60 })
+              res.send({
+                status: 201,
+                msg: '恭喜,注册成功.',
+                data: data
+              })
+            })
           }
         })
       }
@@ -85,10 +136,16 @@ app.post('/reg', upload.array(), (req, res) => {
   })
 })
 
-app.post('/order', upload.array(), (req, res) => {
+app.post('/order', upload.array(), verifyToken,(req, res) => {
   const params = req.body
   connection.query(dbQuery.getUserByPhone, params.phone, (err,_) => {
-    if (err) throw err
+    if (err) {
+      res.send({
+        status: 500,
+        msg: err
+      })
+      res.end()
+    }
     else {
       connection.query(dbQuery.orderInsert, params, (err, result) => {
         if (err) throw err
@@ -96,7 +153,7 @@ app.post('/order', upload.array(), (req, res) => {
           log(result)
           res.send({
             params,
-            status: 0,
+            status: 201,
             message: '订单完成',
             result
           })
@@ -106,14 +163,20 @@ app.post('/order', upload.array(), (req, res) => {
   })
 })
 
-app.get('/order', upload.array(), (req, res) => {
+app.get('/order', upload.array(), verifyToken,(req, res) => {
   console.log(req.query);
   connection.query(dbQuery.orderQuery, req.query, (err, result) => {
-    if(err) throw err
+    if (err) {
+      res.send({
+        status: 500,
+        msg: err
+      })
+      res.end()
+    }
     else {
       res.send({
         params,
-        status: 0,
+        status: 200,
         message: '订单查询成功',
         result
       })
@@ -121,4 +184,18 @@ app.get('/order', upload.array(), (req, res) => {
   })
   res.end()
 })
-app.listen(port, () => console.log(`app listening on port ${port}`)) 
+
+function verifyToken(req, res, next) {
+  const token = req.headers['Authorization']['token'];
+  if (typeof token == 'undefined') {
+    res.sendStatus(403)
+  }else {
+    jwt.verify(token, "seaturtle", function (err, decode) {
+      if (err) {         
+          res.send({'status':403,err});            
+      } else {
+          next()
+      }
+    })
+  }
+}
